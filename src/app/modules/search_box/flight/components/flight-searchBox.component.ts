be460b4e38e52188528d9284b6c6ae1668eb292c
S Mohammad <H.mohammad.alavi@gmail.com>
2019-03-07 18:49:09 +0330

    test

--- src/app/modules/search_box/flight/components/flight-searchBox.component.ts
+++ src/app/modules/search_box/flight/components/flight-searchBox.component.ts
@@ -1,8 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {HttpParams} from '@angular/common/http';
-import {Data} from '@angular/router';
-import {getType} from '@angular/core/src/errors';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-flight-search-box',
@@ -12,33 +11,54 @@ import {getType} from '@angular/core/src/errors';
 
 export class FlightSearchBoxComponent {
 
-  public airportData: any[] = [];
+  public depAirportData: any[] = [];
+  public arrAirportData: any[] = [];
   public airportDepartureBool: boolean = false;
+  public airportArrivalBool: boolean = false;
   public From;
+  public To;
   public departureLabel = '';
+  public arrivalLabel = '';
+  @ViewChild('depAirportList') deplist: ElementRef;
+
 
   constructor(private httpClient: HttpClient) {
   }
 
   /**
    * set airport list
+   *
    * @param e
    */
-  setData(e) {
-    var from = e.target.value;
 
+  setData(e) {
+    //TODO add service fo airport Search
+    var from = e.target.value;
+    var element: HTMLElement;
     var params = new HttpParams().set('from', from);
     if (e.target.value.length > 2) {
       this.httpClient.get('http://e-wallet-online.com/api/v1/airport/search', {params})
         .subscribe((data: any) => {
           if (data['output'] == null) {
             this.airportDepartureBool = false;
-            this.airportData = [];
+            this.depAirportData = [];
           } else {
-            this.airportData = data['output']['airports'];
+            this.depAirportData = data['output']['airports'];
             this.airportDepartureBool = true;
           }
         });
+    }
+
+
+    //If press up and down move in airport list
+
+    if (e.keyCode == 38) {
+      // if (this.deplist.nativeElement.children[0].class === 'active') {
+      this.deplist.nativeElement.style.color = 'red';
+      console.log(typeof this.deplist.nativeElement.children[0]);
+      // }
+    } else if (e.keyCode == 40) {
+      console.log('down');
     }
   }
 
@@ -50,13 +70,38 @@ export class FlightSearchBoxComponent {
     // this.airportDepartureBool = false;
   }
 
-  setDepAirport(e) {
+  setDepAirport(e, t) {
     // Todo Get the code attribute in the best way
-
     this.From = e['path'][2]['dataset']['code'];
-
     this.departureLabel = e['path'][2]['innerText'];
     this.airportDepartureBool = false;
 
+  }
+
+  setArrAirport(e) {
+    // Todo Get the code attribute in the best way
+    this.To = e['path'][2]['dataset']['code'];
+    this.arrivalLabel = e['path'][2]['innerText'];
+    this.airportArrivalBool = false;
+    console.log(this.To);
+  }
+
+  setArrivalAirport(e) {
+    var to = e.target.value;
+    console.log(to);
+    var params = new HttpParams().set('from', this.From).set('to', to);
+    if (e.target.value.length > 2) {
+      this.httpClient.get('http://e-wallet-online.com/api/v1/airport/search', {params})
+        .subscribe((data: any) => {
+          console.log(data);
+          if (data['output'] == null) {
+            this.airportArrivalBool = false;
+            this.arrAirportData = [];
+          } else {
+            this.arrAirportData = data['output']['airports'];
+            this.airportArrivalBool = true;
+          }
+        });
+    }
   }
 }
